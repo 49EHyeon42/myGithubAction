5e8d62b7be1785f16a09fa564077f16a692c6498
49EHyeon42 <49ehyeon42@gmail.com>
2023-12-16 19:23:45 +0900

    init: 프로젝트 시작

---
+++ src/main/java/dev/ehyeon/myGithubAction/MyGithubActionApplication.java
@@ -0,0 +1,12 @@
+package dev.ehyeon.myGithubAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGithubActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGithubActionApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/ehyeon/myGithubAction/MyGithubActionApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.ehyeon.myGithubAction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGithubActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
